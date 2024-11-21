b3f349ddd07986c31cacecf6a2016eee7c2bb54e
Yuber Buitrago <alejo.yb06@gmail.com>
2024-11-21 16:19:00 -0500

    cree propiedades para la clase clVacanteE

--- Entidades/clVacanteE.cs
+++ Entidades/clVacanteE.cs
@@ -7,5 +7,26 @@ namespace HIRE.Entidades
 {
     public class clVacanteE
     {
+
+        public int idVacante { get; set; }
+        public string titulo { get; set; }
+        public string descripcion { get; set; }
+        public string tiempoExperiencia { get; set; }
+        public string salario { get; set; }
+        public string jornada { get; set; }
+        public string horario { get; set; }
+        public string idiomaRequerido { get; set; }
+        public string fechaInicio { get; set; }
+        public string fechaLimite { get; set; }
+        public string fechaPublicacion { get; set; }
+        public string tipoEmpleo { get; set; }
+        public string tipoContrato { get; set; }
+        public string municipio { get; set; }
+        public string idTipoEmpleo { get; set; }
+        public string idTipoContrato { get; set; }
+        public string idMunicipio { get; set; }
+
+
+
     }
 }
