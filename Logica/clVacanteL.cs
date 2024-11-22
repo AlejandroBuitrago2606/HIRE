89483d7475fe92d04eddbb8c051bf124280c24c2
Yuber Buitrago <alejo.yb06@gmail.com>
2024-11-21 22:50:11 -0500

    Cree una clase en la capa de logica para la interaccion con las vacantes y estableci metodos para buscar y listar vacantes.

---
+++ Logica/clVacanteL.cs
@@ -0,0 +1,31 @@
+﻿using HIRE.Datos;
+using HIRE.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HIRE.Logica
+{
+    public class clVacanteL
+    {
+
+        clVacanteD objVacanteD = new clVacanteD();
+
+
+        public List<clVacanteE> mtdBuscarVacante(clVacanteE objVacante, string parametros)
+        {
+
+            List<clVacanteE> objBusqueda = objVacanteD.mtdBuscarVacante(objVacante, parametros);
+            return objBusqueda;
+
+        }
+
+        public clDatosVacante mtdTraerVacante(int idVacante)
+        {
+
+            clDatosVacante objDatosVacante = objVacanteD.mtdTraerVacante(idVacante);
+            return objDatosVacante;
+        }
+    }
+}
