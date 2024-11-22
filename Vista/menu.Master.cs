d723e339f55f234b31f41306aa577a7b0358795d
Yuber Buitrago <alejo.yb06@gmail.com>
2024-11-22 10:03:20 -0500

    agregue a 'else' el procedimiento de cargar los datos del usuario porque interferia al cargar la pagina de busqueda de vacantes

--- Vista/menu.Master.cs
+++ Vista/menu.Master.cs
@@ -24,8 +24,9 @@ namespace HIRE.Vista
 
             if (!IsPostBack)
             {
-                if (ruta.Contains("inicio") && sesion == "false")
+                if (ruta.Contains("inicio") || ruta.Contains("busqueda") && sesion == "false")
                 {
+
 
                     menuLateral.Visible = false;
                     opcionesAdministracion.Visible = false;
@@ -40,37 +41,40 @@ namespace HIRE.Vista
                     pcsAutenticacion.Visible = false;
                     opcionesBusqueda.Visible = false;
 
-                }
+                    clUsuarioL objTraerDatosUsuario = new clUsuarioL();
+                    int idUsuario = int.Parse(Session["idUsuario"].ToString());
+                    clUsuarioE objDatosEnvio = new clUsuarioE();
+                    clUsuarioE objUsuarioE = new clUsuarioE();
+                    objUsuarioE = objTraerDatosUsuario.mtdValidarUsuario(objDatosEnvio = null, idUsuario);
 
 
-                clUsuarioL objTraerDatosUsuario = new clUsuarioL();
-                int idUsuario = int.Parse(Session["idUsuario"].ToString());
-                clUsuarioE objDatosEnvio = new clUsuarioE();
-                clUsuarioE objUsuarioE = new clUsuarioE();
-                objUsuarioE = objTraerDatosUsuario.mtdValidarUsuario(objDatosEnvio = null, idUsuario);
-
-
-                if (objUsuarioE.foto == "")
-                {
-                    txtNombreUsuario.InnerText = objUsuarioE.nombre + " " + objUsuarioE.apellido;
-                    imgFotoPerfil.Src = "recursos/imagenes/perfil.png";
-
-
-                }
-                else
-                {
-                    txtNombreUsuario.InnerText = objUsuarioE.nombre + " " + objUsuarioE.apellido;
-
-                    if (ruta.Contains("inicio") && sesion == "false")
+                    if (objUsuarioE.foto == "")
                     {
+                        txtNombreUsuario.InnerText = objUsuarioE.nombre + " " + objUsuarioE.apellido;
                         imgFotoPerfil.Src = "recursos/imagenes/perfil.png";
+
+
                     }
                     else
                     {
-                        imgFotoPerfil.Src = ResolveUrl("recursos/fotosPerfil/" + objUsuarioE.foto.ToString());
-                        imgFotoPerfil.Alt = "error";
+                        txtNombreUsuario.InnerText = objUsuarioE.nombre + " " + objUsuarioE.apellido;
+
+                        if (ruta.Contains("inicio") && sesion == "false")
+                        {
+                            imgFotoPerfil.Src = "recursos/imagenes/perfil.png";
+                        }
+                        else
+                        {
+                            imgFotoPerfil.Src = ResolveUrl("recursos/fotosPerfil/" + objUsuarioE.foto.ToString());
+                            imgFotoPerfil.Alt = "error";
+                        }
                     }
-                } 
+
+
+                }
+
+
+
             }
         }
 
@@ -81,7 +85,7 @@ namespace HIRE.Vista
             Session.Clear();
             Session.Abandon();
             Response.Redirect("inicio.aspx");
-            
+
         }
     }
 }
