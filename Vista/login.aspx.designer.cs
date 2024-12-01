fd2b721378b0cba5f1416efb6b1fd622d0f34867
Yuber Buitrago <alejo.yb06@gmail.com>
2024-11-30 22:45:33 -0500

    Creacion de formularios para los procesos de autenticacion.
    *recuperarContraseña.aspx, crearCuenta.aspx*
    - Se crearon los formularios para los diferentes procesos de autenticacion
    
    *login.aspx*
    - Se cambiaron las rutas de acceso de algunos elementos para abrir los nuevos formularios.

--- Vista/login.aspx.designer.cs
+++ Vista/login.aspx.designer.cs
@@ -49,14 +49,5 @@ namespace HIRE.Vista
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button btnIniciarSesion;
-
-        /// <summary>
-        /// Control btnRegistrarse.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button btnRegistrarse;
     }
 }
